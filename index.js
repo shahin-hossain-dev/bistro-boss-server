95e4945cef76ede1f100bf3db94efaa4b2e2d997
shahin-hossain-dev <shahin.hossain.dev@gmail.com>
2024-05-17 19:23:32 +0600

    65-8 Load Menu Data From Server

--- index.js
+++ index.js
@@ -34,6 +34,14 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const menuCollection = client.db("bistroDB").collection("menu");
+
+    app.get("/menu", async (req, res) => {
+      const result = await menuCollection.find().toArray();
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
@@ -41,7 +49,7 @@ async function run() {
     );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
