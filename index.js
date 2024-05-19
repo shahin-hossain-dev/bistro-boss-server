3a8b26bb149205960a4fd4ccf7ec42142c49f31a
shahin-hossain-dev <shahin.hossain.dev@gmail.com>
2024-05-20 02:04:24 +0600

    67-5 (Interesting) Show Add To Cart Count Using Refetch

--- index.js
+++ index.js
@@ -49,6 +49,14 @@ async function run() {
       res.send(result);
     });
 
+    app.get("/carts", async (req, res) => {
+      const email = req.query?.email;
+
+      const query = { email };
+      const result = await cartCollection.find(query).toArray();
+      res.send(result);
+    });
+
     app.post("/carts", async (req, res) => {
       const cart = req.body;
       const result = await cartCollection.insertOne(cart);
