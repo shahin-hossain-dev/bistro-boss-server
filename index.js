1a189c0c90d5cdcedd85ba38fe6e46e839f00498
shahin-hossain-dev <shahin.hossain.dev@gmail.com>
2024-05-23 02:33:56 +0600

    68-5 Display All Users And Create Make Admin API

--- index.js
+++ index.js
@@ -60,7 +60,28 @@ async function run() {
       const result = await userCollection.find().toArray();
       res.send(result);
     });
-    // menu api
+
+    app.patch("/users/admin/:id", async (req, res) => {
+      const id = req.params.id;
+
+      const filter = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          role: "admin",
+        },
+      };
+      const result = await userCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+    });
+
+    app.delete("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    // menu related api
 
     app.get("/menu", async (req, res) => {
       const result = await menuCollection.find().toArray();
